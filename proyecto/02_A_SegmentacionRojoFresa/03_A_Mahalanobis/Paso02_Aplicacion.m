bb290c7c56d1901e8f5080aeffe746055755a6f2
Marina <marina.delgado536@alu.uhu.es>
2021-11-02 18:49:40 +0100

    pues resulta que AHORA SÍ esta mahalanobis terminado

--- proyecto/02_A_SegmentacionRojoFresa/03_A_Mahalanobis/Paso02_Aplicacion.m
+++ proyecto/02_A_SegmentacionRojoFresa/03_A_Mahalanobis/Paso02_Aplicacion.m
@@ -11,7 +11,8 @@ numImagenes = 2;
 
 % SELECCIONAR MODELO (escoger entre 1,2,3)
 modeloSeleccionado = 1;
-
+% modeloSeleccionado = 2;
+% modeloSeleccionado = 3;
 
 % CARGAMOS EL MODELO
 modelo = parametrosMahalanobis{modeloSeleccionado,1};
@@ -24,45 +25,12 @@ for i = 1:numImagenes
     % LEEMOS LA IMAGEN
     imagen = imread([ 'EvRojo' num2str(i) '.tif']);
 
+    % APLICAMOS EL CLASIFICADOR DE MAHALANOBIS A LA IMAGEN
+    Ib = aplica_mahalanobis(imagen,modelo,...
+                        centroide,mCov,radio);
 
-    % REDUCIMOS LA IMAGEN
-    imagenR = imresize(imagen,0.5);
-
-    % OBTNEMOS LOS DESCRIPTORES DE INTERES NORMALIZADOS
-    [d1 d2 d3] = calcula_descriptores_interes(imagenR, modelo);
-
-
-    % recorremos cada pixel de la imagen
-    D = zeros(size(d1)); % matriz de detecciones
-    pixel = zeros(1,3);
-
-
-    for f = 1:size(imagenR,1)
-        for c = 1:size(imagenR,2)
-
-            % caracterizamos cada pixel
-            pixel(1,1) = d1(f,c);
-            pixel(1,2) = d2(f,c);
-            pixel(1,3) = d3(f,c);
-
-            % calculamos la distancia Mahalanobis de pixel a centroide
-            distPixelCentroide = distanciaM_punto_a_punto(pixel, centroide,mCov);
-
-            if distPixelCentroide <= radio
-                D(f,c) = 1;
-            else
-                D(f,c) = 0;
-            end
-
-        end
-    end
-
-
-    % Reescalamos la matriz codifImagen para que coincida con las de imagen
-    [F C dim] = size(imagen);
-    codifImagen = round(imresize(D,[F C], 'nearest'));
-    Ib = codifImagen == 1;
-
+    
+    % VISUALIZAMOS LOS RESULTADOS SOBRE LA IMAGEN
     VisualizaColores(imagen,Ib,[0 0 255],true);
 end
 
